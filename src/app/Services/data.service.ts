198c7d8878c0351a4c67ddf32f3ad3ed2ed9fb7f
fatmaelhot <fatmaelhot289@gmail.com>
2024-07-16 06:25:05 +0300

    add data service

--- src/app/Services/data.service.ts
+++ src/app/Services/data.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  private apiUrl = 'http://localhost:3000';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getCustomers(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/customers`);
+  }
+
+  getTransactions(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/transactions`);
+  }
 }
