514afd4b0363279b273fe09e6c3b025904af0887
fatmaelhot <fatmaelhot289@gmail.com>
2024-07-16 06:23:40 +0300

    create server

---
+++ src/app/Components/customer-list/customer-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './customer-list.component.html',
+  styleUrl: './customer-list.component.css'
+})
+export class CustomerListComponent {
+
+}
---
+++ src/app/Components/transaction-graph/transaction-graph.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transaction-graph',
+  standalone: true,
+  imports: [],
+  templateUrl: './transaction-graph.component.html',
+  styleUrl: './transaction-graph.component.css'
+})
+export class TransactionGraphComponent {
+
+}
